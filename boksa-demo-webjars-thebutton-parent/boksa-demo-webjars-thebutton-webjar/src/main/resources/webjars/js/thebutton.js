47b3508cad743b95377ee0c004dc6b14d6bfdbca
Benjamin Boksa <b.boksa@boksa.de>
2019-10-06 13:25:23 +0200

    improve JS example code

--- boksa-demo-webjars-thebutton-parent/boksa-demo-webjars-thebutton-webjar/src/main/resources/webjars/js/thebutton.js
+++ boksa-demo-webjars-thebutton-parent/boksa-demo-webjars-thebutton-webjar/src/main/resources/webjars/js/thebutton.js
@@ -1,12 +1,13 @@
 $(document).ready(function() {
 	
-	$('#thebutton').load("/webjars/thebutton/html/thebutton.html");
-	
-	$('#thebutton').click(function() {
-		$.ajax({
-			url : "/thebutton/click/count"
-		}).then(function(data) {
-			$('#thebutton-message').html(data.message);
+	$('#thebutton').load("/webjars/thebutton/html/thebutton.html", function() {
+		$('#thebutton a.btn').click(function() {
+			$.ajax({
+				url : "/thebutton/click/count"
+			}).then(function(data) {
+				$('#thebutton-message').html(data.message);
+			});
 		});
 	});
+
 });
